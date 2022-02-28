237b554ba6eec2ccebae66699ef3787309da517e
Nichole Boothman <nbooth731@gmail.com>
2022-02-28 17:59:07 -0500

    updated

---
+++ totalpopapp.py
@@ -0,0 +1,46 @@
+from numpy import int64
+from dash import Dash, dcc, html, Output, Input
+import plotly.express as px
+import pandas as pd
+import folium
+
+df = pd.read_csv('formatted_totalpopulation.csv')
+# df.astype({'year': 'int64'})
+df['year'] = df['year'].astype('int64')
+
+app = Dash(__name__)
+
+
+app.layout = html.Div(children=[
+    html.H1(children='Total Population Life Expectancy Change Over Years'),
+    html.Div([
+        dcc.Graph(id='graph-with-slider'),
+        dcc.Slider(
+            df['year'].min(),
+            df['year'].max(),
+            step=None,
+            value=df['year'].min(),
+            marks={str(year): str(year) for year in df['year'].unique()},
+            id='year_slider'
+        )
+    ])
+])
+
+
+@app.callback(
+    Output('graph-with-slider', 'figure'),
+    Input('year_slider', 'value'))
+def update_figure(year_slider):
+    filtered_df = df[df.year == year_slider]
+
+    fig = px.choropleth(filtered_df, locations='name', color='Avg Life Expectancy',
+                        color_continuous_scale="Viridis",
+                        range_color=(0, 12),
+                        labels={'Avg Life Expectancy': 'Life Expectancy'}
+                        )
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    return fig
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
